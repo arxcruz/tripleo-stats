f0e2f8ab205fcf778b9f314ff0500219ebd98627
Arx Cruz <arxcruz@redhat.com>
2018-10-08 18:49:36 +0200

    Adding dependence to charts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,24 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChartDashboardComponent } from './chart-dashboard/chart-dashboard.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavbarComponent
+        NavbarComponent,
+        ChartDashboardComponent
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
-        NgbModule
+        NgbModule,
+        NgxChartsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
