1b316f535b72178ef3d2159f46e258179ca13f39
Arx Cruz <arxcruz@redhat.com>
2018-10-11 16:49:48 +0200

    Adding more functions to jobs service

--- client/src/app/services/jobs.service.ts
+++ client/src/app/services/jobs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { jobs_data } from './data';
+import { jobs_data, job_types } from './data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,16 @@ export class JobsService {
     private _data = jobs_data;
 
     constructor() { }
+
+    getJobTypes() {
+        return job_types;
+    }
+
+    getJobTypeById(job_type_id: number) {
+        return job_types.find(job_type => {
+            return job_type.job_id == job_type_id;
+        });
+    }
 
     getJobByCategory(category_id: number) {
         return this._data.find(jobs => {
