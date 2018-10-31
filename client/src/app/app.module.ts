596ac71cc2c0ca532a777d1e64210cb90cdcacdd
Arx Cruz <arxcruz@redhat.com>
2018-10-31 12:05:40 +0100

    Updating components and modules
    * Update the job-category component
    * Adding HttpClientModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,17 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ChartDashboardComponent } from './components/chart-dashboard/chart-dashboard.component';
 import { JobsListComponent } from './components/jobs-list/jobs-list.component';
-import { JobsCategoryComponent } from './components/jobs-category/jobs-category.component';
+import { JobsTypeComponent } from './components/jobs-type/jobs-type.component';
 import { DefaultComponent } from './components/default/default.component';
 import { JobCategoryButtonsComponent } from './components/job-category-buttons/job-category-buttons.component';
 
-const appRoutes = [
+const appRoutes: Routes = [
     { path: 'main', component: DefaultComponent},
     { path: 'jobs/:category_jobs', component: JobsListComponent },
     { path: '', redirectTo: '/main', pathMatch: 'full' }
@@ -25,7 +26,7 @@ const appRoutes = [
         NavbarComponent,
         ChartDashboardComponent,
         JobsListComponent,
-        JobsCategoryComponent,
+        JobsTypeComponent,
         DefaultComponent,
         JobCategoryButtonsComponent
     ],
@@ -34,6 +35,7 @@ const appRoutes = [
         BrowserModule,
         NgbModule,
         NgxChartsModule,
+        HttpClientModule,
         RouterModule.forRoot(appRoutes)
     ],
     providers: [],
