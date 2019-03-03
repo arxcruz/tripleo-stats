b9e51cad3143f00cad21affc6591a919848ce415
Arx Cruz <arxcruz@redhat.com>
2019-03-03 14:47:31 +0100

    Adding jobs-view component
    This component will have the job-run-list and chart component

---
+++ src/app/components/jobs/jobs-view/jobs-view.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-view',
+  templateUrl: './jobs-view.component.html',
+  styleUrls: ['./jobs-view.component.css']
+})
+export class JobsViewComponent implements OnInit {
+
+    constructor() {
+    }
+
+    isCollapsed = true;
+
+    ngOnInit() {
+
+    }
+}
